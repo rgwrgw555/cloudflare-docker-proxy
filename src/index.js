c6fb4fbaec6f1fb75bd219ada54ae464a486490f
斌 <41340269+rgwrgw555@users.noreply.github.com>
2024-06-12 17:50:14 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.yc-qlwl.tech": "https://registry-1.docker.io",
+  "quay.yc-qlwl.tech": "https://quay.io",
+  "gcr.yc-qlwl.tech": "https://gcr.io",
+  "k8s-gcr.yc-qlwl.tech": "https://k8s.gcr.io",
+  "k8s.yc-qlwl.tech": "https://registry.k8s.io",
+  "ghcr.yc-qlwl.tech": "https://ghcr.io",
+  "cloudsmith.yc-qlwl.tech": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
